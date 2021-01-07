43c115419eb7a9ce9a06d0a73162e74a5350bfe3
FlyingWorkshop <76822456+FlyingWorkshop@users.noreply.github.com>
2021-01-07 00:15:55 -0800

    Update TempleGuide.py

--- TempleGuide.py
+++ TempleGuide.py
@@ -38,20 +38,64 @@ class Temple:
 
 class Database:
     def __init__(self):
-        # cache name + dedication/status of every LDS temple listed on Church website
         url = 'https://www.churchofjesuschrist.org/temples/list?lang=eng'
         source = 'source_cache.json'
         self._cache_source_page(url, source)
 
-        # make a list of Temple objects
         self.temples = []
         self._make_temples(source)  # lat/lng are empty
 
-        # cache Google data for each temple
         dirname = 'google_cache'
         api_key = 'YOURS_HERE'
         self._cache_google_data(api_key, dirname)
         self._add_latlng(dirname)
+
+    @staticmethod
+    def _cache_source_page(url, filename):
+        if os.path.exists(filename):
+            pass
+
+        # get html
+        session = HTMLSession()
+        r = session.get(url)
+        r.html.render()
+        html = r.html.html
+
+        # parse html and cache data
+        cache = {}
+        soup = BeautifulSoup(html, 'lxml')
+        for tag in soup.main('li'):
+            spans = tag('span')
+            name = spans[0].text
+            dedicated = spans[2].text
+            cache[name] = dedicated
+
+        # store cache as json
+        with open(filename, 'w') as f:
+            json.dump(cache, f, indent=4)
+
+    def _make_temples(self, source):
+        data = json.load(open(source))
+        for name, dedicated in data.items():
+            temple = Temple(name, dedicated)
+            self.temples.append(temple)
+
+    def _cache_google_data(self, api_key: str, dirname):
+        if not os.path.exists(dirname):
+            os.mkdir(dirname)
+        for temple in tqdm(self.temples):
+            cache = f'{dirname}/{temple.name}.json'
+            if not os.path.exists(cache):
+                print('Verifying google_cache...')
+                gmaps = googlemaps.Client(api_key)
+                data = gmaps.geocode(temple.name)
+                json.dump(data, open(cache, 'w'), indent=4)
+
+    def _add_latlng(self, dirname):
+        for temple in self.temples:
+            data = json.load(open(f'{dirname}/{temple.name}.json'))[0]
+            temple.lat = data['geometry']['location']['lat']
+            temple.lng = data['geometry']['location']['lng']
 
     def make_bar(self, s: str):
         data = {}
@@ -92,50 +136,3 @@ class Database:
         df = pd.DataFrame(data=data)
         fig = px.scatter_geo(df, lat='lat', lon='lng', color='status', projection='orthographic', hover_name='temple')
         fig.show()
-
-    @staticmethod
-    def _cache_source_page(url, filename):
-        if os.path.exists(filename):
-            pass
-
-        # get html
-        session = HTMLSession()
-        r = session.get(url)
-        r.html.render()
-        html = r.html.html
-
-        # parse html and cache data
-        cache = {}
-        soup = BeautifulSoup(html, 'lxml')
-        for tag in soup.main('li'):
-            spans = tag('span')
-            name = spans[0].text
-            dedicated = spans[2].text
-            cache[name] = dedicated
-
-        # store cache as json
-        with open(filename, 'w') as f:
-            json.dump(cache, f, indent=4)
-
-    def _make_temples(self, source):
-        data = json.load(open(source))
-        for name, dedicated in data.items():
-            temple = Temple(name, dedicated)
-            self.temples.append(temple)
-
-    def _cache_google_data(self, api_key: str, dirname):
-        if not os.path.exists(dirname):
-            os.mkdir(dirname)
-
-        for temple in tqdm(self.temples):
-            cache = f'{dirname}/{temple.name}.json'
-            if not os.path.exists(cache):
-                gmaps = googlemaps.Client(api_key)
-                data = gmaps.geocode(temple.name)
-                json.dump(data, open(cache, 'w'), indent=4)
-
-    def _add_latlng(self, dirname):
-        for temple in self.temples:
-            data = json.load(open(f'{dirname}/{temple.name}.json'))[0]
-            temple.lat = data['geometry']['location']['lat']
-            temple.lng = data['geometry']['location']['lng']
