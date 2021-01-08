6292dd87b708acf44d5c1c1f8ccca6223839f43b
FlyingWorkshop <76822456+FlyingWorkshop@users.noreply.github.com>
2021-01-07 21:23:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,12 @@
 import TempleGuide
-import sys
-
-# TODO: how do you return a datastructure/plot that represents all the temples w/ 'Q' in the name built in 2000
-
-
-def do_plots():
-    while True:
-        plot_type = input('Plot Type (empty string to stop): ')
-        if plot_type == '':
-            break
-        elif plot_type == 'bar':
-            # do something
-            pass
-        elif plot_type == 'map':
-            # do something
-            pass
-        else:
-            print('Only bar graphs ("bar") and maps ("map") are supported.')
+import time
 
 
 def main():
     db = TempleGuide.Database()
-    db.make_bar('y')
-    args = sys.argv[1:]
-
-    num_args = len(args)
-    if num_args < 0 or num_args > 1:
-        print('Please specify the action to take as first argument.')
-        print('Add -s to search temples')
-        print('Add -p to plot temples')
-    else:
-        if args[0] == '-s':
-            pass
-        if args[0] == '-p':
-            do_plots()
-
-
-
 
 
 if __name__ == '__main__':
+    start_time = time.time()
     main()
+    print("--- %s seconds ---" % (time.time() - start_time))
