ceb3ba864d2daedb4d2995aa03443c639b6a2fa5
FlyingWorkshop <76822456+FlyingWorkshop@users.noreply.github.com>
2021-01-13 18:17:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,15 @@
-import TempleGuide
-import plotly_express as px
-import pandas as pd
-
-
-def report(db: TempleGuide.Database):
-    df = pd.DataFrame(data=db.index)
-    print(df)
-
-
-def make_globe(db: TempleGuide.Database):
-    """plots every LDS temple on a 3d model of Earth"""
-    df = pd.DataFrame(data=db.index)
-    fig = px.scatter_geo(df, 'lat', 'lng', color='status', projection='orthographic', hover_name='name')
-    fig.show()
-
-
-def make_bar(db: TempleGuide.Database):
-    """makes a bar graph w/ the number of temples dedicated every year"""
-    data = sorted([(k, len(v)) for k, v in db.inverted['year'].items()], key=lambda t: t[0])
-    df = pd.DataFrame(data, columns=['Year', 'Temples Dedicated'])
-    fig = px.bar(df, 'Year', 'Temples Dedicated')
-    fig.show()
-
-
-def search(db: TempleGuide.Database):
-    df = pd.DataFrame(data=db.index)
-    print(df)
+import TempleGuide as tg
 
 
 def main():
-    db = TempleGuide.Database()
-    # report(db)
-    # make_bar(db)
-    # make_globe(db)
+    db = tg.Database()
+    # tg.display(db)
+    # tg.make_globe(db)
+    # tg.make_bar(db)
+    # print(db.temple_names)        # same as db.data['name']
+    # print(db.temple_countries)    # same as db.data['country']
+    # print(db.temple_statuses)       # same as db.data['status']
 
 
 if __name__ == '__main__':
     main()
-    
