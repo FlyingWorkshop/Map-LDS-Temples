4301b4e72d5e8896ee541ff3d401cc685eaabf99
FlyingWorkshop <76822456+FlyingWorkshop@users.noreply.github.com>
2021-01-13 18:16:44 -0800

    Update TempleGuide.py

--- TempleGuide.py
+++ TempleGuide.py
@@ -1,116 +1,162 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 import googlemaps
+import time
 import json
 import os
+import pandas as pd
+import plotly_express as px
+
+GOOGLE_API_KEY = 'YOURS_HERE'
 
 
 class Temple:
-    def __init__(self, name: str, dedicated: str):
+    def __init__(self, name=None, dedicated=None, country=None, lat=None, lng=None):
         self.name = name
-        self.ded = dedicated
 
-        # lat/lng are added by Database() init
-        self.lat = None
-        self.lng = None
+        # geography (lat/lng/country) filled by google
+        self.country = country
+        self.lat = lat
+        self.lng = lng
 
-        if self.exists():
-            date = dedicated.split()
-            self.month = date[1]
-            self.year = int(date[2])
-            self.status = 'Built'
-        else:
-            self.month = None
-            self.year = None
-            self.status = dedicated  # e.g. 'Construction' 'Announced' 'Renovation'
+        self.status = None
+        self.date = None
+        self._add_ded_info(dedicated)
 
-    def exists(self):
-        return self.ded[0].isnumeric()
+    def _add_ded_info(self, ded):
+        """sets date and status attrs"""
+        if ded and isinstance(ded, str):
+            if ded[0].isnumeric():
+                self.status = 'Built'
+                self.date = time.strptime(ded, '%d %B %Y')[:3]  # tuple (year, month, day)
+            else:
+                self.status = ded  # ex: 'Construction' 'Renovation' 'Announced'
 
+    def __str__(self):
+        return self.name
+
+    def __bool__(self):
+        return self.status == 'Built'
 
 class Database:
-    def __init__(self):
-        webcache = 'lds_cache.json'
-        self._cache_source(webcache)
-
-        gcache_dir = 'google_caches'
+    def __init__(self, webcache='lds_cache.json', geocache_dir='google_caches'):
         self.temples = []
-        self._make_temples(webcache)  # lat/lng are empty
-        self._cache_gdata(gcache_dir, 'YOUR KEY HERE')
-        self._fill_coords(gcache_dir)
+        self._make_temples(webcache, geocache_dir)
+
+        self.data = None
+        self._make_data()
 
         self.index = None
-        self.inverted = None
         self._make_index()
-        self._make_inverted()
 
-    def _cache_source(self, file):
-        if not os.path.exists(file):
-            html = self._get_html('https://www.churchofjesuschrist.org/temples/list?lang=eng')
-            data = self._parse_html(html)
-            with open(file, 'w') as f:
+        self._make_shortcuts()
+
+    def _make_temples(self, wc, gc_dir):
+        self._check_webcache(wc)  # lists temples' name and dedication
+        with open(wc) as f:
+            d1 = json.load(f)
+        for name, ded in d1.items():
+            self._check_geocache(gc_dir, name)
+            with open(f'{gc_dir}/{name}.json') as f:
+                d2 = json.load(f)
+            lat = d2['geometry']['location']['lat']
+            lng = d2['geometry']['location']['lng']
+            country = ''
+            for component in reversed(d2['address_components']):
+                if 'country' in component['types']:
+                    country = component['long_name']
+            self.temples.append(Temple(name=name, dedicated=ded, country=country, lat=lat, lng=lng))
+
+    def _check_webcache(self, wc):
+        if not os.path.exists(wc):
+            url = 'https://www.churchofjesuschrist.org/temples/list?lang=eng'
+            html = self._get_html(url)
+            data = self._parse(html)  # data has temples' name and dedication
+            with open(wc, 'w') as f:
                 json.dump(data, f, indent=4)
 
     @staticmethod
     def _get_html(url):
         session = HTMLSession()
         r = session.get(url)
-        r.html.render()
+        r.html.render()  # renders javascript html and stores it in {obj}.html.html
         return r.html.html
 
     @staticmethod
-    def _parse_html(html):
-        d = {}
+    def _parse(html):
+        result = {}
         soup = BeautifulSoup(html, 'lxml')
         for tag in soup.main('li'):
             spans = tag('span')
             name = spans[0].text  # temple name (ex: 'Aba Nigeria Temple')
-            ded = spans[2].text  # either status (ex: 'Construction') or dedication date (ex: '1 January 2001)
-            d[name] = ded
-        return d
+            ded = spans[2].text   # dedication date (ex: '1 January 2001) or status (ex: 'Construction')
+            result[name] = ded
+        return result
 
-    def _make_temples(self, source):
-        data = json.load(open(source))
-        for name, dedicated in data.items():
-            temple = Temple(name, dedicated)
-            self.temples.append(temple)
+    @staticmethod
+    def _check_geocache(gc_dir, temple_name):
+        geocache = f'{gc_dir}/{temple_name}.json'
+        if not os.path.exists(geocache):
+            print(f'Getting "{geocache}"')
+            if not os.path.exists(gc_dir):
+                os.mkdir(gc_dir)
+            gmaps = googlemaps.Client(GOOGLE_API_KEY)
+            data = gmaps.geocode(temple_name)[0]  # geocode returns a 1-elem list (elem is a multi-layer dict)
+            json.dump(data, open(geocache, 'w'), indent=4)
 
-    def _cache_gdata(self, dirname, api_key):
-        if not os.path.exists(dirname):
-            os.mkdir(dirname)
-        for temple in self.temples:
-            cache = f'{dirname}/{temple.name}.json'
-            if not os.path.exists(cache):
-                print(f'Getting {cache}...')
-                gmaps = googlemaps.Client(api_key)
-                data = gmaps.geocode(temple.name)
-                json.dump(data, open(cache, 'w'), indent=4)
-
-    def _fill_coords(self, dirname):
-        for temple in self.temples:
-            data = json.load(open(f'{dirname}/{temple.name}.json'))[0]
-            temple.lat = data['geometry']['location']['lat']
-            temple.lng = data['geometry']['location']['lng']
+    def _make_data(self):
+        attrs = list(Temple().__dict__)
+        d = {attr: [] for attr in attrs}
+        for o in self.temples:
+            for k, v in o.__dict__.items():
+                d[k].append(v)
+        self.data = d
 
     def _make_index(self):
-        attrs = list(self.temples[0].__dict__)
-        self.index = {attr: [] for attr in attrs}
-        for temple in self.temples:
-            for attr in attrs:
-                inside = self.index[attr]       # list
-                elem = temple.__dict__[attr]    # elem is the value of a given attr for this temple (ex: 'August')
-                inside.append(elem)
-
-    def _make_inverted(self):
-        attrs = list(self.temples[0].__dict__)[-3:]
-        outer = {attr: {} for attr in attrs}  # keys are literals of attrs of class Temple (ex: 'month)
-        for temple in self.temples:
-            for attr in attrs:
-                inner = outer[attr]          # inner is a subdict of outer
-                key = temple.__dict__[attr]  # keys are values of attrs of this Temple object (ex: 'August')
-                if not key:
+        d = {}
+        for o in self.temples:
+            for k, v in o.__dict__.items():
+                if not v or any(k == s for s in [None, 'name', 'lat', 'lng', 'country']):
                     continue
-                if key not in inner:
-                    inner[key] = []
-                inner[key].append(temple.name)
-        self.inverted = outer
+                if k not in d:
+                    d[k] = {}
+                if k == 'date':
+                    v = v[0]
+                if v not in d[k]:
+                    d[k][v] = []
+                d[k][v].append(o.name)
+        self.index = d
+
+    def _make_shortcuts(self):
+        """
+        Dynamically creates attributes for this instance of Database. These attributes are more human-readable
+        shortcuts for the inner values of .data (ex: .temple_countries is the same as .data['country'])
+        >>> db = Database()
+        >>> db.temple_names[:3]
+        ['Aba Nigeria Temple', 'Abidjan Ivory Coast Temple', 'Accra Ghana Temple']
+        """
+        for attr in self.data.keys():
+            shortcut = f'temple_{attr}s'  # ex: temple_names
+            if attr[-1] == 'y':
+                shortcut = f'temple_{attr[:-1]}ies'  # ex: temple_countries
+            if attr[-1] == 's':
+                shortcut = f'temple_{attr}es'  # ex: temple_statuses
+            setattr(self, shortcut, self.data[attr])
+
+
+def display(db: Database):
+    df = pd.DataFrame(db.data)
+    print(df)
+
+
+def make_globe(db: Database):
+    df = pd.DataFrame(db.data)
+    fig = px.scatter_geo(df, lat='lat', lon='lng', color='status', hover_name='name', projection='orthographic')
+    fig.show()
+
+
+def make_bar(db: Database):
+    data = sorted([(k, len(v)) for k, v in db.index['date'].items()], key=lambda t: t[0])
+    df = pd.DataFrame(data, columns=['year', 'temples built'])
+    fig = px.bar(df, x='year', y='temples built')
+    fig.show()
